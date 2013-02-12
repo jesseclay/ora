8e466afb73e42764a17e896b12a3e7e2b5028aa7
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-02-11 16:57:06 -0800

    add now playing data

---
+++ Model/NowPlayingData.m
@@ -0,0 +1,13 @@
+//
+//  NowPlayingData.m
+//  Codename
+//
+//  Created by Brian Holder-Chow Lin On on 2/11/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "NowPlayingData.h"
+
+@implementation NowPlayingData
+
+@end
