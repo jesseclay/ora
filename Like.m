a735288805a3fd823eb7660abb7952c17d4f5eab
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-02-11 16:38:08 -0800

    add like model

---
+++ Like.m
@@ -0,0 +1,13 @@
+//
+//  Like.m
+//  Codename
+//
+//  Created by Brian Holder-Chow Lin On on 2/11/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "Like.h"
+
+@implementation Like
+
+@end
