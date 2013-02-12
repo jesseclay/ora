ef1b8650fbbebf21c8715c31f2651a713729ec8a
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-02-11 16:39:09 -0800

    add user feedback

---
+++ Model/UserFeedback.h
@@ -0,0 +1,13 @@
+//
+//  UserFeedback.h
+//  Codename
+//
+//  Created by Brian Holder-Chow Lin On on 2/11/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserFeedback : NSObject
+
+@end
