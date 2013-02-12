f7dcb1ec10e2603e8df238313fdd13fe72c44f6e
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-02-11 16:53:06 -0800

    create live audio stream

---
+++ Model/LiveAudioStream.h
@@ -0,0 +1,13 @@
+//
+//  LiveAudioStream.h
+//  Codename
+//
+//  Created by Brian Holder-Chow Lin On on 2/11/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LiveAudioStream : NSObject
+
+@end
---
+++ Model/LiveAudioStream.m
@@ -0,0 +1,13 @@
+//
+//  LiveAudioStream.m
+//  Codename
+//
+//  Created by Brian Holder-Chow Lin On on 2/11/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "LiveAudioStream.h"
+
+@implementation LiveAudioStream
+
+@end
