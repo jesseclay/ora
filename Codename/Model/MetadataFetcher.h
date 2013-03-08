570da9d9ac4c97c05e225ebeacae772dc877fe81
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-03-08 02:07:38 -0800

    add metadata fetcher class

---
+++ Codename/Model/MetadataFetcher.h
@@ -0,0 +1,13 @@
+//
+//  MetadataFetcher.h
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MetadataFetcher : NSObject
+
+@end
---
+++ Codename/Model/MetadataFetcher.m
@@ -0,0 +1,13 @@
+//
+//  MetadataFetcher.m
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "MetadataFetcher.h"
+
+@implementation MetadataFetcher
+
+@end
