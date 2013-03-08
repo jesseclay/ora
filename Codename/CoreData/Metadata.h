300bc04cff9df48ac464628974c2bfd590ddda8d
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-03-08 13:43:57 -0800

    create migration with fields to match metadata fetcher

---
+++ Codename/CoreData/Metadata.h
@@ -0,0 +1,22 @@
+//
+//  Metadata.h
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Metadata : NSManagedObject
+
+@property (nonatomic, retain) NSDate * datetime;
+@property (nonatomic, retain) NSString * type;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * artiste;
+@property (nonatomic, retain) NSString * artURLStringMedium;
+@property (nonatomic, retain) NSString * artURLStringLarge;
+
+@end
---
+++ Codename/CoreData/Metadata.m
@@ -0,0 +1,21 @@
+//
+//  Metadata.m
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "Metadata.h"
+
+
+@implementation Metadata
+
+@dynamic datetime;
+@dynamic type;
+@dynamic title;
+@dynamic artiste;
+@dynamic artURLStringMedium;
+@dynamic artURLStringLarge;
+
+@end
