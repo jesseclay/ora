39da781014e3baa1be9c0a8db338467ec6c3a9f8
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-03-08 16:32:36 -0800

    subclass BBCCoreDataMan...

---
+++ Codename/CoreData/ORACoreDataManager.h
@@ -0,0 +1,13 @@
+//
+//  ORACoreDataManager.h
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "BBCCoreDataManager.h"
+
+@interface ORACoreDataManager : BBCCoreDataManager
+
+@end
---
+++ Codename/CoreData/ORACoreDataManager.m
@@ -0,0 +1,20 @@
+//
+//  ORACoreDataManager.m
+//  ORA
+//
+//  Created by Brian Holder-Chow Lin On on 3/8/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "ORACoreDataManager.h"
+
+#define DOCUMENT_NAME @"ORACoreDataDocument"
+
+@implementation ORACoreDataManager
+
++ (NSString *)documentName
+{
+  return DOCUMENT_NAME;
+}
+
+@end
